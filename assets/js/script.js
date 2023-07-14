b7e87f4c208b649af8e4d25a98913659e509f55f
cslunsford <christopher.lunsford@uconn.edu>
2023-07-13 23:24:17 -0400

    added api call and generated response

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,36 +11,61 @@ $(function () {
     });
     function formSubmit() {
         var listContainer = $('#container');
-        var ingredients = $('#ingredients').val();
+        var ingredients = $('#ingredients').val().trim();
         var ingredientList = $('<li>');
-        if (ingredients.trim() === '') {
+        if (ingredients === '') {
             $('#ingredients').val('');
-            ingredients.attr('placeholder', 'Add the ingredients you have here!');
+            $('#ingredients').attr('placeholder', 'Add the ingredients you have here!');
             return;
         }
-        ingredientList.text(ingredients.trim());
-        listContainer.append(ingredientList).addClass(listedItems);
+        ingredientList.text(ingredients);
+        listContainer.append(ingredientList);
         $('#ingredients').val('');
     }
     $(document).on('click', '#generateBtn', function(event) {
         event.preventDefault();
-        displayRecipes();
-    })
+        generateRecipes();
+    });
     
-    var apiUrl = 'https://api.spoonacular.com/recipes/complexSearch';
-    var apiKey = 'b70ec2d513aa1a6ac6fbc26bbc3410906bb8f2be';
-    var recipeContainer = $('#recipeContainer');
-    var mealType = getMealType($('#mealType').val());
-    
-    function getMealType() {
-        if ($('#mealType').val() === '1') {
-            return 'breakfast';
-        }
-        if ($('#mealType').val() === '2') {
-            return 'snack';
-        }
-        if ($('#mealType').val() === '3') {
-            return 'main-course'
-        }
+    function generateRecipes() {
+        var ingredientsArray = [];
+        $('#container li').each(function () {
+            ingredientsArray.push($(this).text().trim());
+        });
+
+        var mealType = $('#mealType').val();
+        
+        fetchRecipes(ingredientsArray, mealType)
+        .then(data => {
+            displayRecipes(data.results)
+        });
     };
+
+    function fetchRecipes(ingredientsArray, mealType) {
+        var apiKey = 'c6db27357d8f494387f67c1e0ada60f5';
+        var apiUrl = 'https://api.spoonacular.com/recipes/complexSearch';
+        var queryString = `apiKey=${apiKey}&includeIngredients=${encodeURIComponent(ingredientsArray.join(','))}&type=${mealType}&addRecipeInformation=true`;
+        var queryUrl = `${apiUrl}?${queryString}`;
+        return fetch(queryUrl)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        });
+    };
+
+    function displayRecipes(recipes) {
+        var recipeContainer = $('#recipeContainer');
+        recipeContainer.empty();
+        for (var i = 0; i < recipes.length; i++) {
+            var recipe = recipes[i];
+            var recipeCard = $('<div>').addClass('recipe-card');
+            var recipeLink = $('<a>').attr('href', recipe.sourceUrl);
+            var recipeImage = $('<img>').attr('src', recipe.image).attr('alt', recipe.title);
+            var recipeTitle = $('<h3>').text(recipe.title);
+            recipeLink.append(recipeImage, recipeTitle)
+            recipeCard.append(recipeLink);
+            recipeContainer.append(recipeCard);
+        }
+    }
+
 })
